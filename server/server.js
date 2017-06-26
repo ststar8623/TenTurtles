930768638ae5311201e019558feaf6f58eddc822
ststar8623 <ststar8623@gmail.com>
2017-06-26 12:19:42 -0700

    food api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/search', (req, res) => {
+  let item = req.body.item;
+  let appID = 'f4b3c424';
+  let appKey = 'f657bcbb2f43a3a73f4c251076307c3b';
+  let url = 'https://api.edamam.com/search/?q=';
+  let options = {
+    method: 'GET',
+    uri: url + item + '&app_id=' + appID + '&app_key=' + appKey,
+    gzip: true
+  };
+  request(options, (err, res, body) => {
+    console.log(body);
+  }).then((result) => {
+    console.log(result);
+  })
 });
 
 app.listen(3000, function() {
