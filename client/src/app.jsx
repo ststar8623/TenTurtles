801fe9d98547f00bfd5d5e231da03decf5ba7e52
Michael Chiang <chiangshtaa@gmail.com>
2017-06-28 14:21:02 -0700

    implement preference selection on client side (#21)

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -9,7 +9,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: ''
+      item: '',
+      prefer: ''
     };
     this.search = this.search.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -23,11 +24,22 @@ class App extends React.Component {
 
   search() {
     console.log('click');
+    let passPref = [];
+    for (let key in this.state.prefer) {
+      if (key !== 'open' && key !== 'preferences') {
+        console.log('true or false', this.state.prefer[key]);
+        if (this.state.prefer[key] === true) {
+          passPref.push(key);
+        }
+      }
+    }
+    console.log('passPref', passPref);
     $.ajax({
       url: '/search',
       method: 'POST',
       data: {
-        item: this.state.item
+        item: this.state.item,
+        choices: passPref
       },
       success: function(data) {
         console.log('success', data);
@@ -35,25 +47,18 @@ class App extends React.Component {
     });
   }
 
-  // search() {
-  //   console.log('click');
-  //   $.ajax({
-  //     url: 'http://138.68.58.133/pairing',
-  //     type: 'POST',
-  //     data: {
-  //       ingredients: [this.state.item]
-  //     },
-  //     success: function(data) {
-  //       console.log('success', data);
-  //     }
-  //   });
-  // }
+  handlePref(childState) {
+    console.log('working appjsx');
+    this.setState({
+      prefer: childState
+    }, () => {console.log(this.state)});
+  }
 
   render() {
     return (
       <Grid style={styles.container}>
         <h1 style={styles.h1}>App 4 Food</h1>
-        <Drop />
+        <Drop handlePreferences={this.handlePref.bind(this)}/>
         <form style={styles.form}>
           <FormControl style={styles.inputBox} bsSize="small" type="text" placeholder="Search here" onChange={this.handleChange} />
           <Button type="button" bsSize="small" onClick={this.search}>Submit</Button>
