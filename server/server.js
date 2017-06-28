31fb19bbac67f34eeb67201b304c680e7eb80309
ststar8623 <ststar8623@gmail.com>
2017-06-27 21:16:05 -0700

    Fully implemented All API's

--- server/server.js
+++ server/server.js
@@ -5,53 +5,62 @@ const request = Promise.promisify(require('request'));
 const recipe = require('./recipeRefactor.js');
 const wine = require('./wineRefactor.js');
 const api = require('./config.js');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.post('/search', (req, res) => {
   let item = req.body.item;
   console.log('item ', item);
-  let options = {
-    url: 'https://api.edamam.com/search?q=' + item,
-    headers: {
-       'app_id': api.recipe_appId,
-      'app_key': api.recipe_appKey
-    }
+  let finalResults = {
+    finalRecipes: [],
+    finalWines: []
   };
-  request(options).then((result) => {
-    return recipe.refactor(JSON.parse(result.body).hits);
-  }).then(result => {
-    // console.log('result: ', result);
-    let wines = ["pinot noir", "grenache", "st. laurent", "carignan", "counoise"];
-    return wines;
-  }).then(pairings => {
-    // console.log(pairings);
-    let random = Math.floor(Math.random() * pairings.length);
-    console.log('random: ', random);
-    let options = {
-         url: 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?sort=popularity|descending&apikey=' + api.wine_key,
-      search: pairings[4]
-    };
-    request(options).then(result => {
-      return wine.refactor(JSON.parse(result.body).Products.List);
-      // console.log('wines: ', wines);
-    }).then(result => {
-      console.log('result[0]::::::::::::::::::::::::::', result);
+  
+  axios.post('https://api.edamam.com/search?q=' + item, { "app_id": api.recipe_appId, "app_key": api.recipe_appkey })
+  .then((result) => {
+    return recipe.refactor(result.data.hits);
+  })
+  .then(recipes => {
+    return recipes.map(rec => {
+      finalResults.finalRecipes.push(rec);
+      return rec.ingredients.map(ingredient => {
+        return ingredient.food;
+      }).join(' ').split(' ');
     });
   })
+  .then(ingredients => {
+    return Promise.map(ingredients, ingredient => {
+      return axios.post('http://138.68.58.133/pairing', {"ingredients": ingredient}).then(result => {
+        return result.data;
+      });
+    });
+  })
+  .then(wines => {
+    return Promise.map(wines, wineArray => {
+      let random = Math.floor(Math.random() * wineArray.length);
+      return axios.get('http://services.wine.com/api/beta2/service.svc/JSON/catalog?sortBy=rating|descending&apikey=' + api.wine_key, {"search": wineArray[random]}).then(result => {
+        return wine.refactor(result.data.Products.List);
+      });
+    });
+  })
+  .then(wines => {
+    wines.map(wine => {
+      finalResults.finalWines.push(wine);
+    });
+  })
+  .then( () => {
+    res.send(finalResults);
+  })
+  .catch(error => {
+    console.log('error: ', error);
+  });
 });
 
 app.listen(3000, function() {
   console.log('Connection established.  Listening on port 3000!');
 });
-
-// curl "https://api.edamam.com/search?q=chicken&app_idf4b3c424=&app_key=f657bcbb2f43a3a73f4c251076307c3b&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free"
-
-// wine pairing 
-// ["pinot noir","grenache","st. laurent","carignan","counoise"]
-
-// wine api
-// http://services.wine.com/api/beta2/service.svc/format/catalog
