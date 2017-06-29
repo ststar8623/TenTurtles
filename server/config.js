5a70a3bed49f34a746c38b43f85b5f6f223c3cce
ststar8623 <ststar8623@gmail.com>
2017-06-29 13:54:31 -0700

    Fix no response from wine API

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const recipe_appkey = 'f657bcbb2f43a3a73f4c251076307c3b';
 const recipe_appId = 'f4b3c424';
 
-const wine_key = '01f19429988a60a5727fa5420827206d';
+const wine_key = 'bd5fc150b65407015db7f4fd6337b947';
 
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/tenturtles/image/upload';
 const CLOUDINARY_UPLOAD_PRESET = 'tenturtles';
