5eb24df13bf93963211b60e56094986b1cef4f32
Steven Leung <ststar8623@gmail.com>
2017-06-28 20:17:40 -0700

    clarifia endpoint (#32)
    * created Procfile for heroku deployment
    
    * Fully implemented All API's
    
    * initial
    
    * Change frontend css
    
    * resolve conflicts
    
    * config.js
    
    * Clarifai API & Cloudinary
    
    * renders recipes and wines to page in list
    
    * Fix package.json dependencies
    
    * clarifai api working with frontend
    
    * Delete config.js
    
    * Fix clarifia api not showing correctly
    
    * config.js
    
    * clarfia bug
    :

---
+++ server/config.js
@@ -0,0 +1,18 @@
+const recipe_appkey = 'f657bcbb2f43a3a73f4c251076307c3b';
+const recipe_appId = 'f4b3c424';
+
+const wine_key = '01f19429988a60a5727fa5420827206d';
+
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/tenturtles/image/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'tenturtles';
+
+const clarifai_key = 'd0b67519e6e841aeb5d6c249a52a29f0';
+
+module.exports = {
+  recipe_appkey: recipe_appkey,
+  recipe_appId: recipe_appId,
+  wine_key: wine_key,
+  CLOUDINARY_UPLOAD_URL: CLOUDINARY_UPLOAD_URL,
+  CLOUDINARY_UPLOAD_PRESET: CLOUDINARY_UPLOAD_PRESET,
+  clarifai_key: clarifai_key
+};
