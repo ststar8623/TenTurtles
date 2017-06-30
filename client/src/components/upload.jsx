5e294d9592a0a3427b03db21832c1e79fd39a414
Alexander Chang <awchang56@gmail.com>
2017-06-29 18:57:23 -0700

    adds image selection buttons and integrates with preferences for search (#43)
    * created Procfile for heroku deployment
    
    * all update merged
    
    * adds overlay for recipe and wine details and basic styling
    
    * adds labels for wine details
    
    * fixes clarifai bug
    
    * styled entire app with bootstrap
    
    * works full screen
    
    * adds padding around app and increases font size
    
    * adds image selection buttons and integrates with preferences for search

--- client/src/components/upload.jsx
+++ client/src/components/upload.jsx
@@ -2,15 +2,53 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 import $ from 'jquery';
+import {Modal, Button, Radio, FormGroup} from 'react-bootstrap';
 const cloud = require('../../../server/config.js');
+const prefHelper = require('../../../server/preferenceRefactor');
 
 export default class Upload extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploadedFileCloudinaryUrl: ''
+      uploadedFileCloudinaryUrl: '',
+      imageScanned: false,
+      imageScanResults: [],
+      selectedImage: ''
     };
     this.clarifai = this.clarifai.bind(this);
+  }
+
+  getInitialState() {
+    return { imageScanned: false };
+  }
+
+  close() {
+    this.setState({
+      imageScanned: false,
+      imageScanResults: []
+    });
+    $.ajax({
+      url: '/search',
+      method: 'POST',
+      data: {
+        item: this.state.selectedImage,
+        choices: prefHelper.preferences(this.props.preferences) || null
+      },
+      success: data => {
+        console.log('success', data);
+        this.props.setPairings(data);
+      }
+    });
+  }
+
+  open() {
+    this.setState({ imageScanned: true });
+  }
+
+  handleChange(e, food) {
+    console.log('handleChange e: ', e.target.value);
+    this.setState({ selectedImage: e.target.value }, this.close);
+
   }
 
   onImageDrop(files) {
@@ -48,25 +86,53 @@ export default class Upload extends React.Component {
         url: this.state.uploadedFileCloudinaryUrl
       },
       success: result => {
-        console.log('result: ', result);
-        this.props.setPairings(result);
+        this.setState({
+          imageScanned: true,
+          imageScanResults: result,
+          selectedImage: result[0]
+        })
+        console.log('clarifai results: ', result);
       }
     });
   }
 
   render() {
     return (
-      <form>
-        <div className="FileUpload">
-          <Dropzone
-            style={styles.dropzone}
-            multiple={false}
-            accept="image/*"
-            onDrop={this.onImageDrop.bind(this)}>
-            <p>tap or click to search an image</p>
-          </Dropzone>
-        </div>
-      </form>
+      <div>
+        <form>
+          <div className="FileUpload">
+            <Dropzone
+              style={styles.dropzone}
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onImageDrop.bind(this)}>
+              <p>tap or click to search an image</p>
+            </Dropzone>
+          </div>
+        </form>
+
+
+        <div>
+          <Modal show={this.state.imageScanned} onHide={this.close.bind(this)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirm Picture</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Select the food in the picture</h4>
+                  {
+                    this.state.imageScanResults.slice(0,5).map((food, i) => {
+                      return <Button style={styles.selectFoodBtn} bsSize="large" key={i} onClick={this.handleChange.bind(this)} value={food}>{food}</Button>
+                    })
+                  }
+            </Modal.Body>
+            <Modal.Footer>
+            </Modal.Footer>
+          </Modal>
+      </div>
+
+
+
+      </div>
     )
   }
 }
@@ -79,6 +145,9 @@ let styles = {
     marginBottom : '10px',
     border : '1px dashed black',
     textAlign : 'center'
+  },
+  selectFoodBtn: {
+    marginRight: '10px'
   }
 }
 
