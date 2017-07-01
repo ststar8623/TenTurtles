e93f737c5a3d46576bfa8444c6f9cfc00bc2b93a
ststar8623 <ststar8623@gmail.com>
2017-06-30 22:24:57 -0700

    Small merge with master

---
+++ client/src/components/drop.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Button, Collapse, Well, Checkbox } from 'react-bootstrap';
+
+class Drop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'vegetarian': false,
+      'vegan': false,
+      'dairy-free': false,
+      'egg-free': false,
+      'peanut-free': false,
+      'alcohol-free': false,
+      'shellfish-free': false,
+      preferences: ['vegetarian', 'vegan', 'dairy-free', 'egg-free', 'peanut-free', 'alcohol-free', 'shellfish-free'],
+      open: false
+    };
+  }
+
+  markedPref(pref) {
+    let selected = pref;
+    let obj = {};
+    obj[selected] = !this.state[selected];
+    this.setState(obj, () => {this.props.handlePreferences(this.state)});
+  }
+
+  render() {
+    return (
+      <div>
+        <Button bsStyle="default" style={styles.dropBox} onClick={ () => this.setState({ open: !this.state.open })}>
+          Dietary Restrictions
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+            {this.state.preferences.map((pref, index) => {
+              return (<Button style={styles.btn} bsSize="large" bsStyle={this.state[pref] ? 'primary' : 'default'}key={index} value={pref} onClick={() => this.markedPref(pref)}>{pref}</Button>)
+            })}
+            </Well>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+let styles = {
+  btn: {
+    margin: '5px'
+  },
+  dropBox: {
+    display: 'inline-block',
+    width: '100%',
+    height: '75px',
+    padding: '6px 10px',
+    fontSize: '25px'
+  }
+}
+
+export default Drop;
